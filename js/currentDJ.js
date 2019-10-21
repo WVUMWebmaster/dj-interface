6bb935b8e32d19666dadd69a6b3621cd52a206a3
WVUMWebmaster <43422881+WVUMWebmaster@users.noreply.github.com>
2019-10-21 17:07:27 -0400

    Add files via upload

---
+++ js/currentDJ.js
@@ -0,0 +1,23 @@
+var refreshMins = 5;
+var msPerMin = 1000 * 60;
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("demo").innerHTML = myObj.currentDJ;
+  }
+};
+
+function callApi() {
+  xmlhttp.open("GET", "https://us-central1-wvum-d6fb8.cloudfunctions.net/getDJ", true);
+  xmlhttp.send();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("State Ready")
+  window.setInterval(function () {
+    callApi();
+  }, refreshMins * msPerMin);
+  callApi();
+});
